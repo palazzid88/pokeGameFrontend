173e60d89b98ced51929dbdf2e8e18bfaee9e038
palazzid88 <palazzid88@gmail.com>
2024-10-01 23:16:48 -0300

    comienzo fase 1 proyecto pokegame en frontend React

---
+++ src/pages/battle.js
@@ -0,0 +1,11 @@
+export default function Battle() {
+    return(
+        <>
+            <div>
+                <h1>
+                    aquí se desarrollará la lógica de las batallas
+                </h1>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <div>
+                <h1>
+                    Iniciando app next HOME
+                </h1>
+            </div>
+        </>
+    )
+}
